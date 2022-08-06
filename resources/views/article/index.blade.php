4f2d6299c29f523fcff43de3a48a3b6795744633
Tomoya Nagaya <24806125+toms3776c@users.noreply.github.com>
2022-08-06 21:22:00 +0900

    tailwindcss追加

---
+++ resources/views/article/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Articles</title>
+</head>
+<body>
+    <h1 class="text-3xl font-bold text-blue-600">記事一覧</h1>
+    <ul>
+        <li class="text-blue-500">あいうえお</li>
+        <li class="text-red-500">かきくけこ</li>
+    </ul>
+
+</body>
+</html>
